91466a92309990a56ec85e034e477dded01e16cb
Joksan Alvarado <joksan.alvarado@gmail.com>
2019-05-21 13:33:38 -0600

    Added modifications to the gnrc_networking example for testing the ipv6 prefix dissemination issue.

--- examples/gnrc_border_router/main.c
+++ examples/gnrc_border_router/main.c
@@ -22,16 +22,46 @@
 
 #include "shell.h"
 #include "msg.h"
+#include "xtimer.h"
+#include "net/ipv6/addr.h"
+#include "net/gnrc/netif/conf.h"
+#include "net/gnrc/netapi.h"
+
+#ifndef UHCPC_ETHOS_IF_ADDRESS
+#error "Please set UHCPC_ETHOS_IF_ADDRESS in the Makefile before compiling this test."
+#endif
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 int main(void)
 {
+    ipv6_addr_t ipv6_addrs[GNRC_NETIF_IPV6_ADDRS_NUMOF];
+    ipv6_addr_t addr;
+    int res;
+
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT border router example application");
+
+    /* wait for the ipv6 prefix to be configured by uhcpc */
+    xtimer_usleep(2000000);
+
+    /* remove the current auto-configured address to make space for the global one */
+    ipv6_addr_from_str(&addr, "fe80::2");
+    res = gnrc_netapi_get(7, NETOPT_IPV6_ADDR, 0, ipv6_addrs, sizeof(ipv6_addrs));
+    for (unsigned i = 0; i < (res / sizeof(ipv6_addr_t)); i++) {
+        /* only remove the address that is not used by uhcpc */
+        if (!ipv6_addr_equal(&ipv6_addrs[i], &addr)) {
+          gnrc_netapi_set(7, NETOPT_IPV6_ADDR_REMOVE, 0, &ipv6_addrs[i], sizeof(ipv6_addrs[i]));
+          break;
+        }
+    }
+
+    /* set the global address to the ethos network segment so it becomes routable */
+    ipv6_addr_from_str(&addr, UHCPC_ETHOS_IF_ADDRESS);
+    gnrc_netapi_set(7, NETOPT_IPV6_ADDR, 64 << 8, &addr, sizeof(addr));
 
     /* start shell */
     puts("All up, running the shell now");
